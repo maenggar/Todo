9fade03325c33edd5e2fcd2876d95111ec202cd2
maenggar <maenggar.map@gmail.com>
2020-01-07 13:15:37 +0700

    [local storage completed]

--- mytodo/src/StoreContext/TaskContext.js
+++ mytodo/src/StoreContext/TaskContext.js
@@ -3,23 +3,25 @@ import React, { createContext, useState, useEffect } from "react";
 export const TaskContext = createContext();
 
 function TaskContextProvider(props) {
-  const [task, setTask] = useState([
-    { id: 1, todo: "Running Test", active: true, complete: true },
-    { id: 2, todo: "Buy Unicorn", active: true, complete: false },
-    { id: 3, todo: "Task no 3", active: false, complete: false }
-  ]);
+  const [task, setTask] = useState(
+    JSON.parse(localStorage.getItem("task")) || [
+      { id: 1, todo: "Running Test", active: true, complete: true },
+      { id: 2, todo: "Buy Unicorn", active: true, complete: false },
+      { id: 3, todo: "Task no 3", active: false, complete: false }
+    ]
+  );
 
   useEffect(() => {
     localStorage.setItem("task", JSON.stringify(task));
   }, [task]);
 
   const addTask = newTodo => {
-    const localData = JSON.parse(localStorage.getItem("task"));
+    //const localData = JSON.parse(localStorage.getItem("task") || []);
 
-    console.log(localData, "dari local data");
+    //console.log(localData, "dari local data");
 
     setTask([
-      ...localData,
+      ...task,
       {
         id: task.length + 1,
         todo: `${newTodo}`,
