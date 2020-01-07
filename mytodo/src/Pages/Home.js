927691df057fd64bff144dcad1e025260d39afca
maenggar <maenggar.map@gmail.com>
2020-01-07 10:21:20 +0700

    First Initial

---
+++ mytodo/src/Pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import ListTask from "./ListTask";
+import Footer from "./Footer";
+
+function List() {
+  return (
+    <div>
+      <Header />
+      <ListTask />
+      <Footer />
+    </div>
+  );
+}
+
+export default List;
---
+++ mytodo/src/Pages/ListTask.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ItemTask from "../components/ItemTask";
+
+function ListTask() {
+  return (
+    <div>
+      <ItemTask />
+    </div>
+  );
+}
+
+export default ListTask;
